0dfb73a6c97c6cc2a8f5aebfc8d6b2cdd900e01d
Fadli Khoirul I <fadli.khoirul123454321@gmail.com>
2025-06-11 11:29:56 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// Simple JavaScript for smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// You could add more functionality here like:
+// - A cart system
+// - Order form
+// - Interactive elements
